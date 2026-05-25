b2229b83f28c611ec91fa9c87f258e50d6c557e7
harsh <harshguptahb055@gmail.com>
2026-05-25 20:48:37 +0530

    made a few changes

--- 07_oop/01_solution.py
+++ 07_oop/01_solution.py
@@ -26,24 +26,100 @@
 
 # Question 3 Create an ElectricCar class that inherits from the Car class and has an additional attribute battery_size.
 
-class Car:
-    def __init__(self, brand, model):
-        self.brand = brand
-        self.model = model
+# class Car:
+#     def __init__(self, brand, model):
+#         self.brand = brand
+#         self.model = model
     
-    def full_name(self):
-        return f"{self.brand} {self.model}"
+#     def full_name(self):
+#         return f"{self.brand} {self.model}"
 
-myCar = Car("Toyota", "Corolla")
+# myCar = Car("Toyota", "Corolla")
 
-class ElectricCar(Car):
-    def __init__(self, brand, model, battery_size):
-        super().__init__(brand, model)
-        self.battery_size = battery_size
+# class ElectricCar(Car):
+#     def __init__(self, brand, model, battery_size):
+#         super().__init__(brand, model)
+#         self.battery_size = battery_size
 
 
-myElectricCar = ElectricCar("Tesla", "Model 3", "75 kWh")
-print(myElectricCar.brand)
-print(myElectricCar.model)
-print(myElectricCar.battery_size)
-print(myElectricCar.full_name())
+# myElectricCar = ElectricCar("Tesla", "Model 3", "75 kWh")
+# print(myElectricCar.brand)
+# print(myElectricCar.model)
+# print(myElectricCar.battery_size)
+# print(myElectricCar.full_name())
+
+
+
+
+# 4 Encapsulation
+# Problem: Modify the Car class to encapsulate the brand attribute, making it private, and provide a getter method for it.
+
+# class Car:
+#     def __init__(self, brand, model):
+#         self.__brand = brand
+#         self.model = model
+    
+#     def get_brand(self):
+#         return self.__brand
+
+#     def full_name(self):
+#         return f"{self.__brand} {self.model}"
+
+# class ElectricCar(Car):
+#     def __init__(self, brand, model, battery_size):
+#         super().__init__(brand, model)
+#         self.battery_size = battery_size
+
+
+# myElectricCar = ElectricCar("Tesla", "Model 3", "75 kWh")
+# # print(myElectricCar.brand)
+# # print(myElectricCar.model)
+# # print(myElectricCar.battery_size)
+# # print(myElectricCar.full_name())
+
+# print(myElectricCar.get_brand())
+
+
+#Polymorphism
+# Problem: Demonstrate polymorphism by defining a method fuel_type in both Car and ElectricCar classes, but with different behaviors.
+
+# class Car:
+#     def __init__(self, brand, model):
+#         self.__brand = brand
+#         self.model = model
+    
+#     def get_brand(self):
+#         return self.__brand
+
+#     def full_name(self):
+#         return f"{self.__brand} {self.model}"
+    
+#     def fuel_type(self):
+#         return "Petrol or Diesel"
+
+# myCar = Car("Toyota", "Corolla")
+
+
+# class ElectricCar(Car):
+#     def __init__(self, brand, model, battery_size):
+#         super().__init__(brand, model)
+#         self.battery_size = battery_size
+
+#     def fuel_type(self):
+#         return "Electricity"
+
+
+# myElectricCar = ElectricCar("Tesla", "Model 3", "75 kWh")
+# # print(myElectricCar.brand)
+# # print(myElectricCar.model)
+# # print(myElectricCar.battery_size)
+# # print(myElectricCar.full_name())
+
+# print(myElectricCar.get_brand())
+# print(myElectricCar.fuel_type())
+# print(myCar.fuel_type())
+
+
+# Class Variables
+# Problem: Add a class variable to Car that keeps track of the number of cars created.
+
