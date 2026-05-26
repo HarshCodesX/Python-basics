4cc1e3565d41300f868e93d9d96b0628625f4018
harsh <harshguptahb055@gmail.com>
2026-05-26 19:47:45 +0530

    added code

--- 07_oop/01_solution.py
+++ 07_oop/01_solution.py
@@ -123,13 +123,45 @@
 # Class Variables
 # Problem: Add a class variable to Car that keeps track of the number of cars created.
 
+# class Car:
+#     total_cars = 0
+
+#     def __init__(self, brand, model):
+#         self.__brand = brand
+#         self.model = model
+#         Car.total_cars += 1 #self.total_cars += 1 would also work but it would create an instance variable instead of modifying the class variable
+    
+#     def get_brand(self):
+#         return self.__brand
+    
+#     def full_name(self):
+#         return f"{self.__brand} {self.model}"
+    
+#     def fuel_type(self):
+#         return "Petrol or Diesel"
+    
+# class ElectricCar(Car):
+#     def __init__(self, brand, model, battery_size):
+#         super().__init__(brand, model)
+#         self.battery_size = battery_size
+
+#     def fuel_type(self):
+#         return "Electricity"
+
+# tesla = ElectricCar("Tesla", "Model 3", "75 kWh")
+# toyota = Car("Toyota", "Corolla")
+# print(tesla.total_cars)
+
+# Static Method
+# Problem: Add a static method to the Car class that returns a general description of a car.
+
 class Car:
+    total_cars = 0
+
     def __init__(self, brand, model):
         self.__brand = brand
         self.model = model
-    
-    def get_brand(self):
-        return self.__brand
+        Car.total_cars += 1 
     
     def full_name(self):
         return f"{self.__brand} {self.model}"
@@ -142,5 +174,5 @@ class ElectricCar(Car):
         super().__init__(brand, model)
         self.battery_size = battery_size
 
-        def fuel_type(self):
-            return "Electricity"
+    def fuel_type(self):
+        return "Electricity"
