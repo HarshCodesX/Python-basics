a0dfd12ab7875cc61b13af93ad7caa1990642de2
harsh <harshguptahb055@gmail.com>
2026-05-26 18:27:12 +0530

    added code

--- 07_oop/01_solution.py
+++ 07_oop/01_solution.py
@@ -123,3 +123,24 @@
 # Class Variables
 # Problem: Add a class variable to Car that keeps track of the number of cars created.
 
+class Car:
+    def __init__(self, brand, model):
+        self.__brand = brand
+        self.model = model
+    
+    def get_brand(self):
+        return self.__brand
+    
+    def full_name(self):
+        return f"{self.__brand} {self.model}"
+    
+    def fuel_type(self):
+        return "Petrol or Diesel"
+    
+class ElectricCar(Car):
+    def __init__(self, brand, model, battery_size):
+        super().__init__(brand, model)
+        self.battery_size = battery_size
+
+        def fuel_type(self):
+            return "Electricity"
