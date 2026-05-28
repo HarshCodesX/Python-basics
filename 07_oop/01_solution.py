4b932d298fec04731fb1c88c918dc51156fcda7f
harsh <harshguptahb055@gmail.com>
2026-05-28 19:27:33 +0530

    added code

--- 07_oop/01_solution.py
+++ 07_oop/01_solution.py
@@ -155,20 +155,67 @@
 # Static Method
 # Problem: Add a static method to the Car class that returns a general description of a car.
 
+# class Car:
+#     total_cars = 0
+
+#     def __init__(self, brand, model):
+#         self.__brand = brand
+#         self.model = model
+#         Car.total_cars += 1 
+    
+#     def full_name(self):
+#         return f"{self.__brand} {self.model}"
+    
+#     def fuel_type(self):
+#         return "Petrol or Diesel"
+    
+#     @staticmethod
+#     def general_description():
+#         return "A car is a means of transport"
+    
+# class ElectricCar(Car):
+#     def __init__(self, brand, model, battery_size):
+#         super().__init__(brand, model)
+#         self.battery_size = battery_size
+
+#     def fuel_type(self):
+#         return "Electricity"
+
+# myCar = Car("Toyota", "Corolla")
+# # print(myCar.general_description()) # this will work if we have self keyword in the method definition
+# print(Car.general_description()) #this is correct way to call a statcic method as no instance is needed to call it, so we can also remove the self keyword from the method definition as it is not needed in a static method
+#Note - when we create static methods, we dont need to put self in the function definition as this method wont be called by any instance of that class
+
+
+# Property Decorators
+# Problem: Use a property decorator in the Car class to make the model attribute read-only.
+
 class Car:
     total_cars = 0
 
     def __init__(self, brand, model):
         self.__brand = brand
-        self.model = model
-        Car.total_cars += 1 
+        self.__model = model
+        Car.total_cars += 1
+
+    def get_brand(self):
+        return self.__brand
     
     def full_name(self):
-        return f"{self.__brand} {self.model}"
+        return f"{self.__brand} {self.__model}"
     
     def fuel_type(self):
         return "Petrol or Diesel"
     
+    @staticmethod
+    def general_description():
+        return "A car is a means of transport"
+    
+    def model(self):
+        return self.__model
+    
+
+#ElectricCar class 
 class ElectricCar(Car):
     def __init__(self, brand, model, battery_size):
         super().__init__(brand, model)
@@ -176,3 +223,10 @@ class ElectricCar(Car):
 
     def fuel_type(self):
         return "Electricity"
+
+myCar = Car("Toyota", "Corolla")
+myCar.model = "Camry" # this will work as model is not a private attribute, but we want to make it read only so we will use property decorator to make it read only
+print(myCar.model) # this will work as model is not a private attribute, but we want to make it read only so we will use property decorator to make it read only, so we can use the getter method to access the model attribute
+print(myCar.brand) # this will not work as brand is a private attribute and we have a getter method for it, so we can use that getter method to access the brand attribute
+print(myCar.get_brand())
+print(myCar.model())
