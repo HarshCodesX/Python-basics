849326d442d61ad6f50767280e25aa7ea9df5456
harsh <harshguptahb055@gmail.com>
2026-06-16 12:19:59 +0530

    coding started

---
+++ 09_error_handling/youtube_manager.py
@@ -0,0 +1,15 @@
+def list_all_videos():
+    pass
+
+while True:
+    print("\n Youtube Manager | choose an option: ")
+    print("1. List all youtube videos ")
+    print("2. Add a youtube video ")
+    print("3. Update a youtube video details ")
+    print("4. Delete a youtube video ")
+    print("5. Exit the app ")
+
+    choice = input("Enter your choice: ")
+    match choice:
+        case "1":
+            list_all_videos()
